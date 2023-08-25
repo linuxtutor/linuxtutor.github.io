bbbfa9cd502025d0a11ba6b1f8105f6b116e9dad
linuxtutor <140965427+linuxtutor@users.noreply.github.com>
2023-08-25 23:33:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-var mn = 0;
-function menu() {
-    if (mn == 0) {
-        document.getElementById("menu").style.display="block";
-        mn = 1;
-    } else {
-        document.getElementById("menu").style.display="none";
-        mn = 0;
+var list = 
+    ["index", 
+    "introduction_to_linux",
+    "linux_installation",
+    "linux_filesystem_structure", 
+    "user_and_group_management", 
+    "file_permissions", 
+    "command_line_interface", 
+    "package_manager", 
+    "bash_scripting", 
+    "conclusion"];
+
+function previous(previous_tuto) {
+    var str = window.location.href;
+    previous_tuto = str.substring(str.lastIndexOf('/')+1, str.lastIndexOf('.'));
+    var tuto = list[list.indexOf(previous_tuto) - 1];
+    if (tuto) {
+        window.location.href = "linuxtutor.github.io/" + tuto + ".html";
     }
 }
 
-function startLearning() {
-    window.location.href = "learn";
+function next(previous_tuto) {
+    var str = window.location.href;
+    previous_tuto = str.substring(str.lastIndexOf('/')+1, str.lastIndexOf('.'));
+    var tuto = list[list.indexOf(previous_tuto) + 1];
+    if (tuto) {
+        window.location.href = "linuxtutor.github.io/" + tuto + ".html";
+    }
 }
 
+var ct = 0;
+function content_table() {
+    if (ct == 0 ) {
+        document.getElementById("content_table").style.display="flex";
+        document.getElementById("content_table").style.paddingTop="40px";
+        ct = 1;
+    } else {
+        document.getElementById("content_table").style.display="none";
+        ct = 0;
+    }
+}
